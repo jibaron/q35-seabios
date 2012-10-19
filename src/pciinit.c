9f0ef2780c58b489d020f6fdb8016b045418891b
Isaku Yamahata <yamahata@valinux.co.jp>
2012-10-19 14:59:15 -0400

    seabios: pci: enable SERR of normal device.
    enable SERR of normal device for AER.
    
    Reviewed-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: Isaku Yamahata <yamahata@valinux.co.jp>
    Signed-off-by: Jason Baron <jbaron@redhat.com>

--- src/pciinit.c
+++ src/pciinit.c
@@ -218,7 +218,8 @@ static void pci_bios_init_device(struct pci_device *pci)
     pci_init_device(pci_class_tbl, pci, NULL);
 
     /* enable memory mappings */
-    pci_config_maskw(bdf, PCI_COMMAND, 0, PCI_COMMAND_IO | PCI_COMMAND_MEMORY);
+    pci_config_maskw(bdf, PCI_COMMAND, 0,
+                     PCI_COMMAND_IO | PCI_COMMAND_MEMORY | PCI_COMMAND_SERR);
 
     /* map the interrupt */
     int pin = pci_config_readb(bdf, PCI_INTERRUPT_PIN);
